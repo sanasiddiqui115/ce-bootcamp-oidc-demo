4b300cae245826b1ab4539ed635311cb4ca46415
Sana Siddiqui <sana.siddiqui115@gmail.com>
2026-05-18 22:18:22 +0200

    base infrastructure

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "ironhack-terraform-demo-UNIQUE12345"
+
+  tags = {
+    Name = "terraform-demo"
+    Env  = "dev"
+  }
+}
